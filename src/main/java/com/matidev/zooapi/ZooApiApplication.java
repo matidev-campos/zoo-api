34f4052dbb50a9b911acf13d5639c4f4e08f334e
matidev-campos <matidev.campos@gmail.com>
2026-02-05 11:56:46 -0300

    Initial project config Spring Boot

---
+++ src/main/java/com/matidev/zooapi/ZooApiApplication.java
@@ -0,0 +1,13 @@
+package com.matidev.zooapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooApiApplication.class, args);
+	}
+
+}
